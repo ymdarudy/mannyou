0caaff5f9dad34e533554b8badade6faeaebf532
yuji yamada <ymdarudy@gmail.com>
2022-07-03 13:00:52 +0900

    feat: BASIC認証（本番環境のみ）追加

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development, :test do
   gem "factory_bot_rails"
   gem "faker"
   gem "launchy"
+  gem "dotenv-rails"
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth if Rails.env.production?
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
