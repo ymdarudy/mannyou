fe9bba9b5f6e65f7022e1a5efcd78841eefc312d
yuji yamada <ymdarudy@gmail.com>
2022-07-12 13:48:52 +0900

    refactor: N＋1問題対処を改良

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   def index
-    @users = User.all.order(:id)
+    @users = User.select(:id, :name, :email, :admin).order(:id).eager_load(:tasks)
   end
 
   def new
